1780571ff8371d48c6fe9653971ce2beade5185b
nchandur <chandur.nischal@gmail.com>
2025-06-27 00:40:37 -0400

    fixed recommend API end point (still needs work)

--- scripts/scrape/scrape.sh
+++ scripts/scrape/scrape.sh
@@ -1,6 +1,6 @@
 echo "starting scraping process"
 
-for i in $(seq 15 18);
+for i in $(seq 21 23);
 do 
     go run scripts/scrape/main.go https://www.goodreads.com/list/show/1.Best_Books_Ever?page=$i
     mongodump --host localhost --port 9001 --db books --out data/backup
